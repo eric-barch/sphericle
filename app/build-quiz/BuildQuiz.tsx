e2193c616ef7adceaeab6bcf44831b576158c746
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2023-12-08 21:47:26 -0500

    Add BuildQuiz page

---
+++ app/build-quiz/BuildQuiz.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import useGoogleLibraries from "@/hooks/use-google-libraries";
+import { useState } from "react";
+
+export default function BuildQuiz() {
+  const [googleLibrariesLoaded, setGoogleLibrariesLoaded] =
+    useState<boolean>(false);
+
+  useGoogleLibraries(() => setGoogleLibrariesLoaded(true));
+
+  return googleLibrariesLoaded ? "quiz builder" : "loading";
+}
---
+++ app/build-quiz/page.ts
@@ -0,0 +1,2 @@
+export * from "./BuildQuiz";
+export { default } from "./BuildQuiz";
