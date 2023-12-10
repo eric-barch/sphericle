fa4365fcac6876c0afbc01a72495fd1fe3a2d6a4
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2023-12-10 12:31:45 -0500

    Preliminary implementation of EditLocationButton

--- components/QuizBuilder/Area.tsx
+++ components/QuizBuilder/Area.tsx
@@ -1,6 +1,8 @@
 import { useQuiz } from "@/components/QuizProvider";
 import { AreaState, LocationType } from "@/types";
 import Sublocations from "./Sublocations";
+import EditLocationButton from "./EditLocationButton";
+import { useState } from "react";
 
 interface AreaProps {
   locationId: string;
@@ -14,12 +16,23 @@ export default function Area({ locationId }: AreaProps) {
     throw new Error("areaState must be of type AREA.");
   }
 
+  const [isRenaming, setIsRenaming] = useState<boolean>(false);
+
   return (
     <>
-      <div
-        className={`w-full p-1 rounded-3xl text-left cursor-pointer bg-gray-600 outline outline-2 outline-red-600`}
-      >
-        {areaState.shortName}
+      <div className="relative">
+        <EditLocationButton
+          locationId={locationId}
+          setIsRenaming={setIsRenaming}
+        />
+        <div
+          id="disclosure-button"
+          className={`w-full p-1 rounded-3xl text-left cursor-pointer bg-gray-600`}
+        >
+          <div className="flex-grow min-w-0 px-7 overflow-hidden text-ellipsis whitespace-nowrap">
+            {areaState.shortName}
+          </div>
+        </div>
       </div>
       <Sublocations className="ml-10" parentId={locationId} />
     </>
