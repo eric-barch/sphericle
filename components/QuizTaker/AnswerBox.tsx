ee501a55a9de87a345dfb7f08b2643b8527ee0e2
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2023-12-11 11:46:08 -0500

    AnswerBox logs correct/wrong

--- components/QuizTaker/AnswerBox.tsx
+++ components/QuizTaker/AnswerBox.tsx
@@ -1,21 +1,56 @@
-import { useQuizDispatch } from "@/components/QuizProvider";
-import { QuizDispatchType } from "@/types";
-import { KeyboardEvent } from "react";
+import { useQuiz, useQuizDispatch } from "@/components/QuizProvider";
+import { AreaState, LocationType, PointState, QuizDispatchType } from "@/types";
+import { ChangeEvent, KeyboardEvent, RefObject, useState } from "react";
 
-export default function AnswerBox() {
+interface AnswerBoxProps {
+  inputRef: RefObject<HTMLInputElement>;
+}
+
+export default function AnswerBox({ inputRef }: AnswerBoxProps) {
+  const quiz = useQuiz();
   const quizDispatch = useQuizDispatch();
+  const takerSelected = quiz.locations[quiz.takerSelectedId] as
+    | AreaState
+    | PointState;
+
+  const [input, setInput] = useState<string>("");
+
+  function checkAnswer() {
+    const normalizedAnswer = (
+      takerSelected.userDefinedName || takerSelected.shortName
+    )
+      .trim()
+      .toLowerCase();
+    const normalizedInput = input.trim().toLowerCase();
+
+    if (normalizedAnswer === normalizedInput) {
+      console.log("Correct!");
+    } else {
+      console.log("WRONG!!!!");
+    }
+
+    inputRef.current.value = "";
+
+    quizDispatch({
+      type: QuizDispatchType.INCREMENT_TAKER_SELECTED,
+    });
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setInput(event.currentTarget.value);
+  }
 
   function handleKeyDown(event: KeyboardEvent<HTMLDivElement>) {
     if (event.key === "Enter") {
-      quizDispatch({
-        type: QuizDispatchType.INCREMENT_TAKER_SELECTED,
-      });
+      checkAnswer();
     }
   }
 
   return (
     <input
+      ref={inputRef}
       className="w-1/2 p-1 rounded-3xl text-left bg-gray-300 text-black border-2 border-gray-600 px-5 focus:outline-none absolute bottom-9"
+      onChange={handleChange}
       onKeyDown={handleKeyDown}
     />
   );
