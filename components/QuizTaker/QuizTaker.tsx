97bbcd808c7041d2deee7d02528553c6d1de3e70
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2024-01-11 22:39:06 -0500

    Refactor TakeQuiz page to match global styling

--- components/QuizTaker/QuizTaker.tsx
+++ components/QuizTaker/QuizTaker.tsx
@@ -10,15 +10,21 @@ import {
   QuizTakerStateDispatchType,
 } from "@/types";
 import * as Dialog from "@radix-ui/react-dialog";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import AnswerBox from "./AnswerBox";
 import { useQuizTakerState } from "./QuizTakerStateProvider";
 import ScoreBox from "./ScoreBox";
+import Loading from "../Loading";
 
-export default function QuizTaker() {
+interface QuizTakerProps {
+  googleLibsLoaded: boolean;
+}
+
+export default function QuizTaker({ googleLibsLoaded }: QuizTakerProps) {
   const { rootId, allFeatures } = useAllFeatures();
   const { quizTakerState, quizTakerStateDispatch } = useQuizTakerState();
 
+  const [mapLoaded, setMapLoaded] = useState<boolean>(false);
   const [displayedFeature, setDisplayedFeature] = useState<
     AreaState | PointState | null
   >(null);
@@ -51,53 +57,71 @@ export default function QuizTaker() {
     setDisplayedFeature(displayedFeature);
   }, [allFeatures, quizTakerState]);
 
-  function handleOpenAutoFocus(event: Event) {
-    event.preventDefault();
-  }
+  const handleMapLoad = useCallback(() => {
+    setMapLoaded(true);
+  }, []);
 
-  function handleClick() {
+  const handleOpenAutoFocus = useCallback((event: Event) => {
+    event.preventDefault();
+  }, []);
+
+  const handleClick = useCallback(() => {
     quizTakerStateDispatch({
       type: QuizTakerStateDispatchType.RESET,
       rootId,
       allFeatures,
     });
-  }
+  }, [rootId, allFeatures, quizTakerStateDispatch]);
 
   return (
-    <div className="h-[calc(100vh-4rem)] relative flex justify-center align-middle content-center">
-      <Dialog.Root
-        open={quizTakerState.remainingFeatureIds.size <= 0}
-        modal={false}
-      >
-        <Dialog.Content
-          className="fixed flex flex-col items-center p-4 bg-white text-black rounded-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40"
-          onOpenAutoFocus={handleOpenAutoFocus}
-        >
-          <Dialog.Title>Quiz Complete!</Dialog.Title>
-          <Dialog.Description className="m-4">{`Your score: ${quizTakerState
-            ?.correctFeatureIds.size} / ${
-            quizTakerState.correctFeatureIds.size +
-            quizTakerState.incorrectFeatureIds.size
-          }`}</Dialog.Description>
-          <Dialog.Close
-            className="p-2 rounded-3xl bg-gray-500 text-white"
-            onClick={handleClick}
+    <>
+      {!googleLibsLoaded || !mapLoaded ? (
+        <Loading className="absolute left-0 right-0 top-0 z-40 bg-gray-700" />
+      ) : null}
+      {googleLibsLoaded ? (
+        <div className="h-[calc(100vh-4rem)] relative flex justify-center align-middle content-center">
+          <Dialog.Root
+            open={quizTakerState.remainingFeatureIds.size <= 0}
+            modal={false}
           >
-            Take Again
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Root>
-      <ScoreBox />
-      <Map
-        mapId="8777b9e5230900fc"
-        displayedFeature={displayedFeature}
-        displayMode={DisplayMode.QUIZ_TAKER}
-      />
-      <AnswerBox
-        displayedFeature={displayedFeature}
-        inputRef={answerBoxInputRef}
-        disabled={quizTakerState.remainingFeatureIds.size <= 0}
-      />
-    </div>
+            <Dialog.Content
+              className="fixed flex flex-col items-center p-4 bg-white text-black rounded-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40"
+              onOpenAutoFocus={handleOpenAutoFocus}
+            >
+              <Dialog.Title>Quiz Complete!</Dialog.Title>
+              <Dialog.Description className="m-4">{`Your score: ${quizTakerState
+                ?.correctFeatureIds.size} / ${
+                quizTakerState.correctFeatureIds.size +
+                quizTakerState.incorrectFeatureIds.size
+              }`}</Dialog.Description>
+              <Dialog.Close
+                className="p-2 rounded-3xl bg-gray-500 text-white"
+                onClick={handleClick}
+              >
+                Take Again
+              </Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Root>
+          <ScoreBox />
+          <Map
+            onLoad={handleMapLoad}
+            padding={{
+              top: 25,
+              bottom: 100,
+              left: 25,
+              right: 25,
+            }}
+            mapId="8777b9e5230900fc"
+            displayedFeature={displayedFeature}
+            displayMode={DisplayMode.QUIZ_TAKER}
+          />
+          <AnswerBox
+            displayedFeature={displayedFeature}
+            inputRef={answerBoxInputRef}
+            disabled={quizTakerState.remainingFeatureIds.size <= 0}
+          />
+        </div>
+      ) : null}
+    </>
   );
 }
