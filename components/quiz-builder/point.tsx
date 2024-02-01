c2ab7372287fceb0ba9d96aa0a32cdec227a42b0
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2024-02-01 10:57:43 -0500

    Make Point select on click rather than focus

--- components/quiz-builder/point.tsx
+++ components/quiz-builder/point.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { PointState, QuizBuilderStateDispatchType } from "@/types";
-import { FocusEvent } from "react";
+import { FocusEvent, MouseEvent } from "react";
 import EditFeatureButton from "./edit-feature-button";
 import FeatureName from "./feature-name";
 import { useQuizBuilderState } from "./quiz-builder-state-provider";
+import { Button } from "../ui/button";
 
 interface PointProps {
   pointState: PointState;
@@ -13,28 +14,26 @@ interface PointProps {
 export default function Point({ pointState }: PointProps) {
   const { quizBuilderState, quizBuilderStateDispatch } = useQuizBuilderState();
 
-  function handleFocus(event: FocusEvent<HTMLDivElement>) {
-    if (!event.currentTarget.contains(event.relatedTarget)) {
-      quizBuilderStateDispatch({
-        dispatchType: QuizBuilderStateDispatchType.SET_SELECTED,
-        featureState: pointState,
-      });
-    }
+  function handleClick(event: MouseEvent<HTMLButtonElement>) {
+    quizBuilderStateDispatch({
+      dispatchType: QuizBuilderStateDispatchType.SET_SELECTED,
+      featureState: pointState,
+    });
   }
 
   return (
-    <div className="relative" onFocus={handleFocus}>
+    <div className="relative">
       <EditFeatureButton featureState={pointState} />
-      <div
+      <Button
         className={`w-full p-1 cursor-pointer rounded-2xl text-left bg-gray-600 ${
           pointState.featureId === quizBuilderState.selectedFeatureId
             ? "outline outline-2 outline-red-700"
             : ""
         }`}
-        tabIndex={0}
+        onClick={handleClick}
       >
         <FeatureName featureState={pointState} />
-      </div>
+      </Button>
     </div>
   );
 }
