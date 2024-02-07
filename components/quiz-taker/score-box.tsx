e4abe622fe98c37721838e057b1f0fa16029146c
Eric Barch <ericbarch@Erics-MBP.lan>
2024-02-07 11:36:36 -0500

    Refactor ScoreBox

--- components/quiz-taker/score-box.tsx
+++ components/quiz-taker/score-box.tsx
@@ -1,7 +1,13 @@
 import { useQuizTakerState } from "./quiz-taker-state-provider";
 
-function ScoreBox() {
-  const { quizTakerState } = useQuizTakerState();
+const ScoreBox = () => {
+  const {
+    quizTakerState: {
+      correctFeatureIds,
+      incorrectFeatureIds,
+      remainingFeatureIds,
+    },
+  } = useQuizTakerState();
 
   return (
     <div className="absolute top-5 right-5 rounded-3xl z-10 bg-gray-500 bg-opacity-80 p-3">
@@ -9,26 +15,20 @@ function ScoreBox() {
         <tbody>
           <tr>
             <td className="pr-4">Correct:</td>
-            <td className="text-right">
-              {quizTakerState.correctFeatureIds.size}
-            </td>
+            <td className="text-right">{correctFeatureIds.size}</td>
           </tr>
           <tr>
             <td className="pr-4">Incorrect:</td>
-            <td className="text-right">
-              {quizTakerState.incorrectFeatureIds.size}
-            </td>
+            <td className="text-right">{incorrectFeatureIds.size}</td>
           </tr>
           <tr>
             <td className="pr-4">Remaining:</td>
-            <td className="text-right">
-              {quizTakerState.remainingFeatureIds.size}
-            </td>
+            <td className="text-right">{remainingFeatureIds.size}</td>
           </tr>
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export { ScoreBox };
