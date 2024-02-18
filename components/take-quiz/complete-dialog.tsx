ec8d9bd74f4d2ca470a65e9fc1da14b9d11ba7e8
Eric Barch <eric.michael.barch@gmail.com>
2024-02-18 18:32:27 -0500

    Clean up AnswerBox

--- components/take-quiz/complete-dialog.tsx
+++ components/take-quiz/complete-dialog.tsx
@@ -1,26 +1,22 @@
 import { useQuizTaker } from "@/providers";
 import * as Dialog from "@radix-ui/react-dialog";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 type CompleteDialogProps = {
   handleReset: () => void;
 };
 
 const CompleteDialog = ({ handleReset }: CompleteDialogProps) => {
-  const {
-    quizTaker: {
-      correctIds: correctFeatureIds,
-      incorrectIds: incorrectFeatureIds,
-      remainingIds: remainingFeatureIds,
-    },
-  } = useQuizTaker();
+  const { quizTaker } = useQuizTaker();
+
+  const { correctIds, incorrectIds, remainingIds } = quizTaker;
 
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    const isComplete = remainingFeatureIds.size === 0;
+    const isComplete = remainingIds.size === 0;
     setIsOpen(isComplete);
-  }, [remainingFeatureIds.size]);
+  }, [remainingIds.size]);
 
   const handleClose = () => {
     setIsOpen(false);
@@ -31,10 +27,8 @@ const CompleteDialog = ({ handleReset }: CompleteDialogProps) => {
       <Dialog.Content className="fixed flex flex-col items-center p-4 bg-gray-500 bg-opacity-80 text-white rounded-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40">
         <Dialog.Title>Quiz Complete!</Dialog.Title>
         <Dialog.Description className="m-4">{`Your score: ${
-          correctFeatureIds.size
-        } / ${
-          correctFeatureIds.size + incorrectFeatureIds.size
-        }`}</Dialog.Description>
+          correctIds.size
+        } / ${correctIds.size + incorrectIds.size}`}</Dialog.Description>
         <Dialog.Close
           className="p-2 mb-1 w-3/4 rounded-3xl bg-gray-700 text-white"
           onClick={handleReset}
